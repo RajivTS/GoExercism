0f89d66de0313392200c0661883477220e481b95
RajivTS <rajshar.email@gmail.com>
2021-12-19 23:11:57 +0530

    Raindrops Exercism

---
+++ raindrops/raindrops.go
@@ -0,0 +1,21 @@
+package raindrops
+
+import "strconv"
+
+func Convert(number int) string {
+	var message string
+	if number%3 == 0 {
+		message += "Pling"
+	}
+	if number%5 == 0 {
+		message += "Plang"
+	}
+	if number%7 == 0 {
+		message += "Plong"
+	}
+	if message == "" {
+		return strconv.Itoa(number)
+	} else {
+		return message
+	}
+}
